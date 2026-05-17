d39fcc2a61a84cfa5b2cf2bf1c03863c1c41f85b
FranForge <fjesus.fjhh@gmail.com>
2026-05-17 12:06:59 +0200

    feat: GET Endpoint con query de categoria y subcategoria

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -17,14 +17,32 @@ namespace api.Controllers
             _context = context;
         }
 
-        // GET: api/products
+        // GET: api/products?category=someSubcategory&subcategory=someSubcategory
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts([FromQuery] string? category, 
+        [FromQuery] string? subcategory)
         {
-            // Usamos ToListAsync() en lugar de ToList()
-            var products = await _context.Products.ToListAsync();
-            return Ok(products);
+            //Creamos una consulta base que se puede ir filtrando según los parametros proporcionados
+            var queryParams = _context.Products.AsQueryable();
+
+            //Agrega el filtro de categoria si se proporciona
+            if(!string.IsNullOrEmpty(category))
+            {
+                queryParams = queryParams.Where(p => p.Category.ToLower() == category.ToLower());
+            }
+
+            //Agrega el filtro de subcategoria si se proporciona
+            if(!string.IsNullOrEmpty(subcategory))
+            {
+                queryParams = queryParams.Where(p => p.Subcategory.ToLower() == subcategory.ToLower());
+            }
+
+            //Ejecuta la consulta con los filtros aplicados y devuelve los resultados
+            var filteredProducts = await queryParams.ToListAsync();
+
+            return Ok(filteredProducts);
         }
+        
 
         // GET: api/products/{id}
         [HttpGet("{id}")]
